e66c53d25218b30224652e4d3e3d60ae9b434f7a
javirrof <javirofe96@gmail.com>
2026-02-21 09:49:41 +0100

    feat: add smart-contract-account v1 post

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const BASE_URL = 'https://blog.techsmachine.com'
 
-export const CATEGORIES = ["tooling", "tokenization"] as const
+export const CATEGORIES = ["tooling", "development", "tokenization"] as const
 
 export type PostCategory = typeof CATEGORIES[number]
